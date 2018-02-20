08083b4b6665a5f54f3069daa4814539612cfe26
niko132 <nikokirste@hotmail.de>
2018-02-20 22:14:30 +0100

    Add files via upload

--- web/api.php
+++ web/api.php
@@ -1,7 +1,7 @@
 <?php
 	$ch = curl_init(); 
 	// set url 
-	curl_setopt($ch, CURLOPT_URL, "https://euw1.api.riotgames.com/lol/summoner/v3/summoners/by-name/" . $_POST["test"] . "?api_key=RGAPI-8e2051b6-62f3-4f28-bca8-5e47d31b29e8"); 
+	curl_setopt($ch, CURLOPT_URL, "https://euw1.api.riotgames.com/lol/summoner/v3/summoners/by-name/" . $_POST["summoner"] . "?api_key=RGAPI-8e2051b6-62f3-4f28-bca8-5e47d31b29e8"); 
 	//return the transfer as a string 
 	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); 
 	// $output contains the output string 
