2eefa86f97fed8e8f69d4c73fabbff769bd6bbc6
niko132 <nikokirste@hotmail.de>
2018-02-20 22:45:16 +0100

    Add files via upload

--- web/api.php
+++ web/api.php
@@ -1,13 +1,30 @@
 <?php
-	$ch = curl_init(); 
-	// set url 
-	curl_setopt($ch, CURLOPT_URL, "https://euw1.api.riotgames.com/lol/summoner/v3/summoners/by-name/" . $_POST["summoner"] . "?api_key=RGAPI-8e2051b6-62f3-4f28-bca8-5e47d31b29e8"); 
-	//return the transfer as a string 
-	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); 
-	// $output contains the output string 
-	$output = curl_exec($ch); 
-	// close curl resource to free up system resources
-	curl_close($ch);
-	
-	echo $output;
+	if (!empty($_POST["summonerName"])) {
+		$summonerName = $_POST["summonerName"];
+		$ch = curl_init(); 
+		// set url 
+		curl_setopt($ch, CURLOPT_URL, "https://euw1.api.riotgames.com/lol/summoner/v3/summoners/by-name/" . $summonerName . "?api_key=RGAPI-8e2051b6-62f3-4f28-bca8-5e47d31b29e8"); 
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+		$output = curl_exec($ch);
+		curl_close($ch);
+		echo $output;
+	} else if (!empty($_POST["accountId"]) && !empty($_POST["gameIndex"])) {
+		$accountId = $_POST["accountId"];		
+		$gameIndex = $_POST["gameIndex"];
+		
+		if (is_numeric($gameIndex)) {
+			$gameIndexInt = (int) $gameIndex;
+			$gameEndIndex = gameIndexInt + 20;
+		
+			$ch = curl_init(); 
+			$url = "https://acs.leagueoflegends.com/v1/stats/player_history/EUW1/" . $accountId . "?begIndex=" . $gameIndexInt . "&endIndex=" . $gameEndIndex;
+			curl_setopt($ch, CURLOPT_URL, $url); 
+			curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+			$output = curl_exec($ch);
+			curl_close($ch);
+			echo $output;
+		} else {
+			echo "gameIndex is no valid int";
+		}
+	}
 ?>
