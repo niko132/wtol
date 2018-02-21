7db064c9343fb1163945410b5ac6d5f7c2828b5b
niko132 <nikokirste@hotmail.de>
2018-02-21 22:46:03 +0100

    Add files via upload

--- web/api.php
+++ web/api.php
@@ -32,5 +32,8 @@
 		} else {
 			echo "gameIndex is no valid int";
 		}
+	} else if (isset$_POST["test"]) {
+		$db = pg_connect("host=ec2-54-247-95-125.eu-west-1.compute.amazonaws.com port=5432 dbname=d6ekuvikv3vj2u user=nsepsdirjogngr password=e9717b306d08ce71e6971bc0a987dc49163401e51ec9f70eb25c6d84c72c15a2");
+		$result = pg_query("INSERT INTO test (test1) VALUES (400)");
 	}
 ?>
