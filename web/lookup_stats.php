bdafca71fad70258aa03a3ac03ce3516f470faa6
niko132 <nikokirste@hotmail.de>
2018-02-22 01:19:14 +0100

    Add files via upload

--- web/lookup_stats.php
+++ web/lookup_stats.php
@@ -2,7 +2,7 @@
 	if (isset($_POST["summonerName"]) && !empty($_POST["summonerName"])) {
 		$db = pg_connect("host=ec2-54-247-95-125.eu-west-1.compute.amazonaws.com port=5432 dbname=d6ekuvikv3vj2u user=nsepsdirjogngr password=e9717b306d08ce71e6971bc0a987dc49163401e51ec9f70eb25c6d84c72c15a2");
 		$summonerName = $_POST["summonerName"];
-		$result = pg_query("SELECT count(*) FROM wtol WHERE summonerName = '" . $summonerName . "'");
+		$result = pg_query("SELECT count(*) FROM wtol WHERE \"summonerName\" = '" . $summonerName . "'");
 		
 		echo $result;
 		echo "nice";
