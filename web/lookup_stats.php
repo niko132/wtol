6deab16d4f5f1c7849fb88291472c0ee626108c4
niko132 <nikokirste@hotmail.de>
2018-02-22 13:39:07 +0100

    Add files via upload

--- web/lookup_stats.php
+++ web/lookup_stats.php
@@ -6,19 +6,48 @@
 		
 		$count = pg_num_rows($result);
 		
+		$accountId = 0;
+		$gameIndex = 0;
+		$totalTime = 0;
+		
 		if ($count > 0) { // this summoner is already in the database
 			$row = pg_fetch_row($result);
 			$accountId = $row[0];
-			$gameCount = $row[1];
-			$summonerName = $row[2];
-			$totalTime = $row[3];
+			$totalTime = (int) $row[1];
+			$lastGameIndex = (int) $row[3];
 			
-			echo $accountId;
-			echo $gameCount;
-			echo $summonerName;
-			echo $totalTime;
+			$gameIndex = $lastGameIndex + 1;
 		} else { // we have to get all the games now :/
+			// TODO: get the accountId
+		}
+		
+		$fetchedGames = 0;
 			
+		do {
+			$games = getGames($accountId, $gameIndex);
+			if ($games) {
+				$jsonGames = json_decode($games);
+				$gamesArray = jsonGames->games->games;
+				$fetchedGames = count($gamesArray);
+				
+				for ($i = 0; i < $fetchedGames; $i++) {
+					$totalTime += $gamesArray[i]->gameDuration;
+				}
+				
+				$gameIndex += $fetchedGames;
+			}
+			
+			$fetchedGames = 0;
+		} while($fetchedGames >= 20);
+		
+		$lastGameIndex = $gameIndex - 1;
+		
+		if ($count > 0) { // update existing
+			$query = "INSERT INTO wtol VALUES (" . $accountId . ", " . $totalTime . ", '" . $summonerName . "', " . $lastGameIndex . ")";
+			$result = pg_query($query);
+		} else { // insert new
+			$query = "UPDATE wtol SET 'lastGameIndex'=" . $lastGameIndex . ", 'totalTime'=" . $totalTime . " WHERE 'accountId'=" . $accountId;
+			$result = pg_query($query);
 		}
 		
 		// Speicher freigeben
@@ -26,5 +55,28 @@
 		
 		// Verbindung schließen
 		pg_close($db);
+		
+		echo $totalTime;
+	}
+	
+	function getGames($accountId, $gameIndex) {
+		if (is_numeric($gameIndex)) {
+			$begGameIndex = (int) $gameIndex;
+			$endGameIndex = $gameIndex + 20;
+			
+			$ch = curl_init(); 
+			$url = "https://acs.leagueoflegends.com/v1/stats/player_history/EUW1/" . $accountId . "?begIndex=" . $begGameIndex . "&endIndex=" . $endGameIndex;			
+			curl_setopt($ch, CURLOPT_URL, $url); 
+			curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+			$output = curl_exec($ch);
+			$httpcode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+			curl_close($ch);
+			
+			if ($httpcode == 200) {
+				return $output;
+			}
+		}
+		
+		return FALSE;
 	}
 ?>
